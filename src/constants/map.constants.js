1f6cccc779eaa19a1b35e880d82df1a786ee8f4f
vlad <vlad.miakshin@gmail.com>
2020-02-28 16:50:26 +0200

    add constants

---
+++ src/constants/map.constants.js
@@ -0,0 +1,2 @@
+export const MAP_CONTAINER_ID = 'map';
+export const MAP_DATA_URL = 'https://cors-anywhere.herokuapp.com/https://israel-sky.s3-eu-west-1.amazonaws.com/';
--- src/constants/option.constants.js
+++ src/constants/option.constants.js
@@ -1,5 +1,5 @@
-export const CLOUDS_OPT = 'Clouds';
+export const CLOUDS_OPT = 'TCDC';
 export const SUNRISE_OPT = 'Sunrise';
 export const SUNSET_OPT = 'Sunset';
 export const THUNDER_OPT = 'Thunder';
-export const RAIN_OPT = 'Rain';
+export const RAIN_OPT = 'APCP';
