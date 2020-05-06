9bab955b6fef7ce898dc8d58f9d0fbf03065edc8
Yevhen Piotrovskyi <yevhen.piotrovskyi@gmail.com>
2020-05-06 13:52:43 +0300

    fix warnings

--- src/components/Footer/Footer.component.js
+++ src/components/Footer/Footer.component.js
@@ -3,13 +3,13 @@ import './Footer.styles.css';
 import { Layout } from 'antd';
 
 const Footer = () => (
-  <Layout.Footer class="footer">
+  <Layout.Footer className="footer">
     (c) Idea and algorithms - Pavel Bernshtam, website and design -{' '}
-    <a target="_blank" HREF="https://yojji.io">Yojji</a>, forecast data -{' '}
-    <a target="_blank" HREF="http://www.ims.gov.il/IMSEng/All_tahazit/Model%20based%20weather%20charts/">
+    <a target="_blank" href="https://yojji.io" rel="noopener noreferrer">Yojji</a>, forecast data -{' '}
+    <a target="_blank" href="http://www.ims.gov.il/IMSEng/All_tahazit/Model%20based%20weather%20charts/" rel="noopener noreferrer">
       Israel Meteorological Service
     </a>{' '}
-    and <a target="_blank" HREF="https://openweathermap.org/api">OpenWeather</a>
+    and <a target="_blank" href="https://openweathermap.org/api" rel="noopener noreferrer">OpenWeather</a>
   </Layout.Footer>
 );
 
