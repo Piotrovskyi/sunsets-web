13785a4383ddab847a02ac08e8d5174e7d115c5d
Pavel Bernshtam <pavelb@cgen.com>
2020-06-13 13:57:37 +0300

    start from wheather tab

--- src/store/ui/ui.reducers.js
+++ src/store/ui/ui.reducers.js
@@ -1,9 +1,9 @@
-import { MAP_PHOTO_SPOTS_TAB_KEY } from '../../constants/map.constants';
+import { MAP_WEATHER_TAB_KEY } from '../../constants/map.constants';
 import createReducer from '../../utils/createReducer';
 import { SET_ACTIVE_MAP_TAB } from './ui.constants';
 
 const initialState = {
-  activeMapTab: MAP_PHOTO_SPOTS_TAB_KEY,
+  activeMapTab: MAP_WEATHER_TAB_KEY,
 };
 
 const ui = createReducer(
